e7b9b06969ef60f2d48598e681d38a40228f23ee
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2023-05-13 19:56:17 +0900

    add vgg backbones

--- cam/backbones/__init__.py
+++ cam/backbones/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 from cam.backbones.mobilenet_v3 import backbone_mobilenet_v3
+from cam.backbones.vgg import (
+    backbone_vgg11,
+    backbone_vgg13,
+    backbone_vgg16,
+    backbone_vgg19,
+)
 
 __all__ = [
     "backbone_mobilenet_v3",
+    "backbone_vgg11",
+    "backbone_vgg13",
+    "backbone_vgg16",
+    "backbone_vgg19",
 ]
